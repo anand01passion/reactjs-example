a7d454d851b44bd9948bb1819e6eec245fc9de72
Ramratan Gupta <ramratan.gupta@knexusgroup.com>
2016-04-13 12:15:57 +0530

    fianl ace editor

--- javascript/app.js
+++ javascript/app.js
@@ -1,13 +1,31 @@
 'use strict';
 var React = require('react');
-
+var AceEditor  = require('react-ace-wrapper');
+let brace  = require('brace');
+require('brace/mode/javascript');
+require('brace/mode/css');
+require('brace/theme/github');
+require('brace/theme/twilight');
 var Example = React.createClass({
+    changeEditorContent : function(val){
+        this.setState({editor_value:val});
+    },
+    getInitialState : function(){
+        return {editor_value : ""};
+    },
     render: function () {
-        return (
-                <div className = "knx-loading" >
-               Hello world!
-                < /div>
-                )
+        var editor_mode = "css";
+        var editor_skin = 'github';
+        return (<div >
+               <AceEditor
+                                        mode={editor_mode}
+                                        theme={editor_skin}
+                                        onChange={this.changeEditorContent}
+                                        name="ace_editor"
+                                        width="100%"
+                                        value={this.state.editor_value} 
+                                    />
+                < /div>);
     }
 });
 
